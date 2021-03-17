e856d0eaf8b96ff21565456fd40f383f852862c6
RobertoPotato <robertwabuti@gmail.com>
2021-03-17 11:33:23 +0300

    Added a few comments

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void _connectToServer() {
     try {
       // Socket Configuration
+      //TODO Change URL
       IO.Socket socket = IO.io("http://192.168.43.58:3000/", <String, dynamic>{
         'transports': ['websocket'],
         'autoConnect': false,
@@ -52,15 +53,22 @@ class _MyHomePageState extends State<MyHomePage> {
       //expose socket outside function
       _socket = socket;
 
-      // Handle socket events
+      // TODO HANDLE SOCKET EVENTS
+      // Server responds to emitted counter value it receives by broadcasting it
+      // To all connected sockets. This method catches that response and prints
+      // the data received from it
+
       socket.on('counterResponse', (data) {
         print(data);
       });
+
+      // ... More Events ...
     } catch (e) {
       print(e);
     }
   }
 
+  // Sends the current value of the counter via socket.io to the server
   void _emmitCounterValue() {
     try {
       _socket.emit('counter', _counter);
@@ -73,9 +81,12 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     // TODO: implement initState
     super.initState();
+
+    // Call function to attempt to connect to server
     _connectToServer();
   }
 
+  // The rest of this is the startup counter app with some minor changes
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -96,6 +107,9 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+
+      // Added a second FAB. It calls the function to emit the counter's value
+      // when pressed
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -112,9 +126,9 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
 
-      // Center the FAB
-      floatingActionButtonLocation: FloatingActionButtonLocation
-          .centerFloat, // This trailing comma makes auto-formatting nicer for build methods.
+      // Positioning the FAB
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
