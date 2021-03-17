86b61a1ff81efc38ebbcad8b6cdfb26ebeeec201
RobertoPotato <robertwabuti@gmail.com>
2021-03-17 11:39:09 +0300

    Few more changes

---
+++ lib/demo/README.dart
@@ -0,0 +1,18 @@
+/*
+* This is built off the flutter counter startup project.
+*
+* Functionality
+*
+* A counter is set to increment a given int value. Pressing the + button does
+* this
+*
+* Pressing the other button with the send icon will emit a message to server
+* with the current value of the server
+*
+* The server will then respond by sending back this value which will be
+* printed on the on the run tab when app is running
+*
+* server can be run with npm run dev
+*
+*
+*/
